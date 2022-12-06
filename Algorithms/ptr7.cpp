a04f9e6be8ac956db6d5ce609e292263f132d356
Elisaveta Malkova <116960519+Myrtilles@users.noreply.github.com>
2022-12-06 18:19:00 +0500

    Add files via upload

---
+++ Algorithms/ptr7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin >> x;
+    int *ptr = &x;
+    cin >> y;
+    *ptr = y;
+    cout << *ptr;
+    
+    
+    return 0;
+}
+
