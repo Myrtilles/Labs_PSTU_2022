df24d82e9a86679db3eb2c079dd19b4416e14bb1
Elisaveta Malkova <116960519+Myrtilles@users.noreply.github.com>
2023-02-05 23:13:24 +0500

    Add files via upload

---
+++ 8QUEENS.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+using namespace std;
+
+int board[8][8];
+
+const int Queen = -1;
+
+void resetBoard();
+void display();
+bool checkQueen(int i);
+void setQueen(int i, int j);
+
+void resetBoard()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            board[i][j] = 0;
+        }
+    }
+}
+
+void display()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[i][j] == Queen)
+            {
+                cout << "Q ";
+            }
+            else
+            {
+                cout << ". ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void setQueen(int i, int j)
+{
+    for (int x = 0; x < 8; ++x)
+    {
+        board[x][j] += 1; // vert attack is marked
+        board[i][x] += 1; // hor attack is marked
+        
+        int d;
+        d = j - i + x;         // diagonal 1
+        if (d >= 0 && d < 8)
+        { board[x][d] += 1; }
+
+        d = j + i - x;        // diagonal 2
+        if (d >= 0 && d < 8)
+        { board[x][d] += 1; }
+    }
+    board[i][j] = Queen;
+}
+
+void resetQueen(int i, int j)
+{
+    for (int x = 0; x < 8; ++x)
+    {
+        board[x][j] -= 1; 
+        board[i][x] -= 1;
+        
+        int d;
+        d = j - i + x; 
+        if (d >= 0 && d < 8)
+        { board[x][d] -= 1; }
+
+        d = j + i - x; 
+        if (d >= 0 && d < 8)
+        { board[x][d] -= 1; }
+    }
+    board[i][j] = 0;
+}
+
+// looking at number of a line
+bool checkQueen(int i)
+{
+    bool result = false;
+
+    for (int j = 0; j < 8; ++j)
+    {
+        if (board[i][j] == 0)
+        {
+            setQueen(i, j); 
+
+            if (i == 7)
+            {
+                result = true;
+            }
+            else if (!(result = checkQueen(i + 1)))
+            {
+                resetQueen(i, j);
+            }
+        }
+        if (result) { break; }
+    }
+    return result;
+}
+
+int main()
+{
+    resetBoard();
+    checkQueen(0);
+    display();
+    return 0;
+}
+
+
+
