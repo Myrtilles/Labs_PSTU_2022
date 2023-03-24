730f64bda86fa60055917c22e3c2a324de251e78
Elisaveta Malkova <116960519+Myrtilles@users.noreply.github.com>
2023-03-24 13:33:23 +0500

    Add files via upload

---
+++ 2Semester/linearsearch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size, key;
+    bool flag = false;
+    cout << "Enter size of an array:" << ' ';
+    cin>> size;
+    int mass[size];
+    for(int i = 0;i < size;i++)
+    {
+        mass[i] = rand() % 101;
+        cout << mass[i] << ' ';
+    }
+    cout<<"enter elements to be searched :" << ' ';
+    cin >> key;
+    for(int i = 0; i < size;i++)
+    {
+        if(key == mass[i])
+        {
+            cout << key << " is found with index "<< i <<endl;
+            flag = true;
+        }
+    }
+    if(!flag)
+    {
+        cout << key << " is not found.";
+    }
+    return 0;
+}
