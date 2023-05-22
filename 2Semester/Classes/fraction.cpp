983239944e62ebf3cb5c948da45157110378a911
Elisaveta Malkova <116960519+Myrtilles@users.noreply.github.com>
2023-05-22 12:53:09 +0500

    Add files via upload

---
+++ 2Semester/Classes/fraction.cpp
@@ -0,0 +1,21 @@
+#include "fraction.h"
+void fraction::Init(double F, int S)
+{
+	first = F;
+	second = S;
+}
+void fraction::Read()
+{
+	cout << "\nfirst?"; cin >> first;
+	cout << "\nsecond?"; cin >> second;
+}
+void fraction::Show()
+{
+	cout << "\nfirst=" << first;
+	cout << "\nsecond=" << second;
+	cout << "\n";
+}
+double fraction::Element(int j)
+{
+	return first * pow(second, j);
+}
